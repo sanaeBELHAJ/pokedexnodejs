4eeffdd2c41f017369b7097418baf1055924012f
sanae <sanai-son112@hotmail.fr>
2018-02-13 12:34:46 +0100

    Initialisation de projet

---
+++ main.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+app.get('/pokemons',function(req, res){
+    
+});
+
+app.get('/pokemons/:id',function(req, res){
+    
+});
+
+app.get('',function(req, res){
+    
+});
+
+app.get('',function(req, res){
+    
+});
+
+app.get('',function(req, res){
+    
+});
+
+app.get('',function(req, res){
+    
+});
+        
+        
+  
